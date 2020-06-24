4b36225824b1bd76b2d4f9700575cca58332340e
Alina Gladkova <a-g1997@yandex.ru>
2020-06-24 13:15:50 +0300

    Back to the top button

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
+$(".top-btn").removeClass("visible");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-    mybutton.style.display = "block";
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 200) {
+    $(".top-btn").addClass("visible");
   } else {
-    mybutton.style.display = "none";
+    $(".top-btn").removeClass("visible");
   }
-}
+});
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+$(".top-btn").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 1000);
+});
